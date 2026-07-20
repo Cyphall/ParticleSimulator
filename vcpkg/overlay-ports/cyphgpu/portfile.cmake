4c030c6dd6dd1baf1c99d6cea545a3632d4482c1
Cyphall <cyphallbraus@gmail.com>
2026-07-20 21:01:05 +0200

    Updated CyphGPU

--- vcpkg/overlay-ports/cyphgpu/portfile.cmake
+++ vcpkg/overlay-ports/cyphgpu/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO Cyphall/CyphGPU
-	REF "08e1b159676137a8885df0cbbc4ad09db5115f99"
-	SHA512 3b2b7ee7779286014780c0ba5ff795e450fb317287aa1ab43d16195f7578274524d0751ba16d6dd5c6f33d7c505f1272ccf8e528cfcd7c9e1a98af1ea155eddc
+	REF "31ada77fbb193594b08d3f8e6457ec4926cae430"
+	SHA512 cbb690bec2cdfac6cf2e466531f50dc1615927515b64dc33cd7eb0c5ea5d95b4469a4e37a385266c74d25f3b30637235be54f85ae3fad376becbf934174678f2
 	HEAD_REF master
 )
 
@@ -23,5 +23,6 @@ vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/CyphGPU")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
